e695bbecf01de6f1f716c26a4fc05acf8f98a7ed
letsmovetocloud <letsmovetocloud@gmail.com>
2022-12-02 08:42:47 +0530

    cidr block range details changed

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/azure"
+      source  = "hashicorp/aws"
       
     }
   }
@@ -11,13 +11,13 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" 
-  region  = "ap-south-1"
+  region  = "us-cnetral"
 }
 
 #Resource Block
 resource "aws_instance" "ec2testserver" {
   ami           = "ami-041db4a969fe3eb68" 
-  instance_type = "B2S"
+  instance_type = "t3.micro"
 
 }
 
@@ -29,9 +29,9 @@ module "vpc" {
 
   # VPC Basic Details
   name = "vpc-dev"
-  cidr = "10.0.0.0/16"   
+  cidr = "192.168.0.0/16"   
   azs                 = ["us-east-1a"]
-  private_subnets     = ["10.0.1.0/24"]
-  public_subnets      = ["10.0.101.0/24"]
+  private_subnets     = ["192.168.1.0/24"]
+  public_subnets      = ["192.168.101.0/24"]
 
 #new comment added 
